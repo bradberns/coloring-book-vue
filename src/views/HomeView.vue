449f29014431ec5cb50dbe2800ca57137a2301b6
bradbernsxxx <bradbernssd@gmail.com>
2024-11-20 15:13:15 -0800

    11-20-2024 Image Adjustment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,23 +8,23 @@
     <div class="mx-1">
       <div class="gridSet">
         <div class="gridCell">
-          <img class="" height="200" width="156"
+          <img class="" height="250" width="195"
               src="https://res.cloudinary.com/dxbedpcbe/image/upload/v1732052999/21_dyqpff.png"/>
         </div>
         <div class="gridCell">
-          <img class="" height="200" width="156"
+          <img class="" height="250" width="195"
               src="https://res.cloudinary.com/dxbedpcbe/image/upload/v1732052998/20_kpxvd9.png"/>
         </div>
         <div class="gridCell">
-          <img class="" height="200" width="156"
+          <img class="" height="250" width="195"
               src="https://res.cloudinary.com/dxbedpcbe/image/upload/v1732052581/18_u1svcx.png"/>
         </div>
         <div class="gridCell">
-          <img class="" height="200" width="156"
+          <img class="" height="250" width="195"
               src="https://res.cloudinary.com/dxbedpcbe/image/upload/v1732052580/17_i7khkp.png"/>
         </div>
         <div class="gridCell">
-          <img class="" height="200" width="156"
+          <img class="" height="250" width="195"
               src="https://res.cloudinary.com/dxbedpcbe/image/upload/v1732049349/16_tugsdj.png"/>
         </div>
       </div>
@@ -33,24 +33,24 @@
 
       <div class="gridSet">
         <div class="gridCell">
-          <img class="" height="200" width="156"
+          <img class="" height="250" width="195"
             src="https://res.cloudinary.com/dxbedpcbe/image/upload/v1732140150/piclumen-1732056625315_mo2x6a.png"/>
         </div>
         <div class="gridCell">
-          <img class="" height="200" width="156"
+          <img class="" height="250" width="195"
             src="https://res.cloudinary.com/dxbedpcbe/image/upload/v1732140147/piclumen-1732056134661_yh9miv.png"/>
         </div>
         <div class="gridCell">
-          <img class="" height="200" width="156"
+          <img class="" height="250" width="195"
             src="https://res.cloudinary.com/dxbedpcbe/image/upload/v1732140144/piclumen-1732055381249_zxa02k.png"/>
         </div>
         <div class="gridCell">
           <img class=""
             src="https://res.cloudinary.com/dxbedpcbe/image/upload/v1732140142/piclumen-1732055116114_luiuq9.png"
-            height="200" width="156"/>
+            height="250" width="195"/>
         </div>
         <div class="gridCell">
-          <img class="" height="200" width="156"
+          <img class="" height="250" width="195"
             src="https://res.cloudinary.com/dxbedpcbe/image/upload/v1732140139/piclumen-1732054301607_kdqu7y.png"/>
         </div>
       </div>
